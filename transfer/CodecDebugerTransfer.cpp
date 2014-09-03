12d55099224059c694f6afaadae4a355ffb9f8e4
isxxguo <isxxguo@pptv.com>
2014-09-03 13:20:07 +0800

    CodecDebugerTransfer dump StreamInfo
    Change-Id: I6127eb43e5b10c1e9705414c80bd4d9a8a2612e8

--- transfer/CodecDebugerTransfer.cpp
+++ transfer/CodecDebugerTransfer.cpp
@@ -19,15 +19,33 @@ namespace ppbox
         void CodecDebugerTransfer::transfer(
             StreamInfo & info)
         {
+            std::cout << "track: " << info.index
+                << "\t type: " << ppbox::avbase::FourCC::to_string(info.type)
+                << "\t sub_type: " << ppbox::avbase::FourCC::to_string(info.sub_type)
+                << "\t format_data: " << info.format_data.size() << " bytes" 
+                << std::endl;
+            if (info.type == StreamType::VIDE) {
+                std::cout << "  width: " << info.video_format.width
+                    << " height: " << info.video_format.height
+                    << " frame_rate: " << info.video_format.frame_rate_num << "/" << info.video_format.frame_rate_den
+                    << std::endl;
+            } else {
+                std::cout << "  channel_count: " << info.audio_format.channel_count
+                    << " sample_size: " << info.audio_format.sample_size
+                    << " sample_rate: " << info.audio_format.sample_rate
+                    << " block_align: " << info.audio_format.block_align
+                    << " sample_per_frame: " << info.audio_format.sample_per_frame
+                    << std::endl;
+            }
             boost::system::error_code ec;
-            debuger_->reset(info, ec);
+            if (debuger_)
+                 debuger_->reset(info, ec);
             num_ = 0;
         }
 
         void CodecDebugerTransfer::transfer(
             Sample & sample)
         {
-            boost::system::error_code ec;
             std::cout << "track: " << sample.itrack
                 << " # " << num_++
                 << "\t time: " << sample.time 
@@ -35,7 +53,9 @@ namespace ppbox
                 << "\t dts: " << sample.dts 
                 << "\t cts: " << sample.dts + sample.cts_delta 
                 << std::endl;
-            debuger_->debug(sample, ec);
+            boost::system::error_code ec;
+            if (debuger_)
+                debuger_->debug(sample, ec);
         }
 
     } // namespace mux
